3326e267d3cf4cc4c84551436df606bfb571fe6d
AlizeeFaytre <afaytre@juniorisep.com>
2017-04-30 18:47:49 +0200

    Ajout de la page d'accueil pour les eleves, communement appelee Vue d'ensemble, ainsi que tous les assets necessaire a cette page

---
+++ src/main/java/com/grille/controller/AccueilEleveController.java
@@ -0,0 +1,17 @@
+package com.grille.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by alizeefaytre on 30/04/2017.
+ */
+
+@Controller
+public class AccueilEleveController {
+
+    @GetMapping("/accueilEleve")
+    public String accueil(){
+        return "eleves/accueilEleve";
+    }
+}
