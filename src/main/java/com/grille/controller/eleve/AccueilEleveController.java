6e7c8c738d0d87a600765fcec0acee2a7e94a6d5
AlizeeFaytre <afaytre@juniorisep.com>
2017-05-07 15:44:05 +0200

    creation d'un service qui permet de recuperer le groupe de l'utilisateur, le semestre courant, la liste des competence avec les notes associées, du coup allegement du controlle AccueilEleveController

--- src/main/java/com/grille/controller/eleve/AccueilEleveController.java
+++ src/main/java/com/grille/controller/eleve/AccueilEleveController.java
@@ -5,6 +5,7 @@ import com.grille.entities.Evaluate;
 import com.grille.entities.Groupe;
 import com.grille.entities.Skill;
 import com.grille.entities.User;
+import com.grille.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -22,50 +23,16 @@ import java.util.Set;
 public class AccueilEleveController {
 
     private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/accueilEleve")
     public String accueil(@AuthenticationPrincipal UserDetails userDetails, Model model){
         User currentUser = userRepository.findByIdentifiant(userDetails.getUsername());
-        Set<Evaluate> listEvaluate = currentUser.getListEvaluate();
-        for (Evaluate e:listEvaluate) {
-            Skill skill2 = e.getSkill();
-            for (Evaluate evaluate:listEvaluate) {
-                Skill skill1 = evaluate.getSkill();
-                if(skill1.equals(skill2)){
-                    if (e.getDate().after(evaluate.getDate())){
-                        listEvaluate.remove(evaluate);
-                    }
-                    else{
-                        listEvaluate.remove(e);
-                    }
-                }
-
-            }
-        }
+        Set<Evaluate> listEvaluate = userService.getLastEvaluate(currentUser);
 
         model.addAttribute("listEvaluate", listEvaluate);
 
-        Calendar cal = Calendar.getInstance();
-        Integer year = cal.get(Calendar.YEAR);
-        String yearStr = Integer.toString(year);
-        int month = cal.get(Calendar.MONTH);
-        String semester = new String();
-        if(month>=2 && month<=8){
-            semester = "S1";
-        }
-        else{
-            semester = "S2";
-        }
-
-        String currentSemester = yearStr + " - " + semester;
-
-        Groupe currentGroup = new Groupe();
-        Set<Groupe> listGroupe = currentUser.getGroupes();
-        for (Groupe g:listGroupe) {
-            if(g.getSemester().equals(currentSemester)){
-                currentGroup = g;
-            }
-        }
+        Groupe currentGroup = userService.getCurrentGroupe(currentUser);
 
         model.addAttribute("groupe", currentGroup);
 
---
+++ src/main/java/com/grille/service/UserService.java
@@ -0,0 +1,69 @@
+package com.grille.service;
+
+import com.grille.entities.Evaluate;
+import com.grille.entities.Groupe;
+import com.grille.entities.Skill;
+import com.grille.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Created by alizeefaytre on 07/05/2017.
+ */
+
+@Service
+public class UserService {
+
+    public Set<Evaluate> getLastEvaluate(User user){
+        Set<Evaluate> listEvaluate = user.getListEvaluate();
+        for (Evaluate e:listEvaluate) {
+            Skill skill2 = e.getSkill();
+            for (Evaluate evaluate:listEvaluate) {
+                Skill skill1 = evaluate.getSkill();
+                if(skill1.equals(skill2)){
+                    if (e.getDate().after(evaluate.getDate())){
+                        listEvaluate.remove(evaluate);
+                    }
+                    else{
+                        listEvaluate.remove(e);
+                    }
+                }
+
+            }
+        }
+        return listEvaluate;
+    }
+
+    public Groupe getCurrentGroupe(User user){
+        String currentSemester = getCurrentSemester();
+        Groupe currentGroup = new Groupe();
+        Set<Groupe> listGroupe = user.getGroupes();
+        for (Groupe g:listGroupe) {
+            if(g.getSemester().equals(currentSemester)){
+                currentGroup = g;
+            }
+        }
+
+        return currentGroup;
+    }
+
+    public String getCurrentSemester(){
+        Calendar cal = Calendar.getInstance();
+        Integer year = cal.get(Calendar.YEAR);
+        String yearStr = Integer.toString(year);
+        int month = cal.get(Calendar.MONTH);
+        String semester = new String();
+        if(month>=2 && month<=8){
+            semester = "S1";
+        }
+        else{
+            semester = "S2";
+        }
+
+        String currentSemester = yearStr + " - " + semester;
+
+        return currentSemester;
+    }
+}
