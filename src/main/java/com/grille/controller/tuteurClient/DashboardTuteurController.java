c828a08e5444667819b020ba3fda8b7d86dd12f7
Jiawdft <jiawei@hotmail.fr>
2017-05-16 10:58:20 +0200

    update

--- src/main/java/com/grille/controller/tuteurClient/DashboardTuteurController.java
+++ src/main/java/com/grille/controller/tuteurClient/DashboardTuteurController.java
@@ -1,7 +1,9 @@
 package com.grille.controller.tuteurClient;
 
 import com.grille.dao.GroupeRepository;
+import com.grille.dao.UserRepository;
 import com.grille.entities.Groupe;
+import com.grille.entities.Role;
 import com.grille.entities.User;
 import com.grille.service.UserRestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -23,9 +28,12 @@ public class DashboardTuteurController {
     private UserRestService userRestService;
     @Autowired
     private GroupeRepository groupeRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/dashboard-tuteur")
-    public String index (Model model, HttpSession session){
+
+    @RequestMapping(value = "/dashboard-tuteur", method = RequestMethod.GET)
+    public String index (Model model, HttpSession session, @RequestParam("groupe")int id){
 
         //selection tous les groupe
         List<Groupe> listGroupe = groupeRepository.findAll();
@@ -52,9 +60,23 @@ public class DashboardTuteurController {
             listListUser.add(listUser);
         }
 
+        //Utilisateur du groupe selected
+        ArrayList<User> listGroupeUsers = new ArrayList<User>();
+        Groupe selectedGroupe = new Groupe();
+        for (Groupe g : listGroupe){
+            if (g.getId() == id){
+                selectedGroupe = g;
+                Set<User> groupeUsers = g.getListUser();
+                listGroupeUsers = new ArrayList<User>(groupeUsers);
+                break;
+            }
+        }
 
+
+        model.addAttribute("listGroupeUsers", listGroupeUsers);
         model.addAttribute("listGroupe",listGroupe);
         model.addAttribute("listListUser", listListUser);
+        model.addAttribute("selectedGroupe",selectedGroupe);
 
 
         return "tuteur-client/dashboard-tuteur";
---
+++ src/main/resources/static/js/dashboard_tuteur.js
@@ -0,0 +1,26 @@
+/**
+ * Created by jiawei on 15/05/2017.
+ */
+
+
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+        return null;
+    }
+    else{
+        return results[1] || 0;
+    }
+}
+
+var condition = $.urlParam('groupe');
+console.log(condition);
+
+
+if (condition != 0){
+    $(window).on('load',function(){
+        $('#modal_visualisation_groupe_user').modal('show');
+    });
+}
+
