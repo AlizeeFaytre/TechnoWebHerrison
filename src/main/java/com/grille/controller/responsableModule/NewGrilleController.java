5522fca93530c0455f5f12da0fba13f192518912
ThomasRichardISEP <tho-richard@sfr.fr>
2017-05-16 17:11:06 +0200

    modifications

--- src/main/java/com/grille/controller/responsableModule/NewGrilleController.java
+++ src/main/java/com/grille/controller/responsableModule/NewGrilleController.java
@@ -5,9 +5,12 @@ import com.grille.dao.SkillRepository;
 import com.grille.entities.Domain;
 import com.grille.entities.Skill;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -49,6 +52,29 @@ public class NewGrilleController {
         model.addAttribute("listSkill", listSkill);
         return "respoModule/new_grille";
     }
+/*
+    @RequestMapping(value = "/new_domain", method = RequestMethod.POST)
+    public ResponseEntity<Void> processXml(@RequestBody String requestBody){
+
+        //selection tous les domain
+        List<Domain> listDomain = domainrepo.findAll();
+
+        //selection max id
+        int maxId = 0;
+        for (Domain d : listDomain){
+            if (d.getId()> maxId){
+                maxId = d.getId();
+            }
+        }
+        maxId = maxId + 1;
+        Domain test = new Domain();
+        test.setId(maxId);
+        test.setName(requestBody);
+        domainrepo.save(test);
+
+        return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+
+    }*/
 
 
 }
