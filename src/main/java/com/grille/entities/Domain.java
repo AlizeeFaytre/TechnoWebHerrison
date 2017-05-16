af29f9e6cd67664401e8db4fd121b0ede536e361
ThomasRichardISEP <tho-richard@sfr.fr>
2017-05-16 13:49:37 +0200

    test insertion

--- src/main/java/com/grille/entities/Domain.java
+++ src/main/java/com/grille/entities/Domain.java
@@ -43,6 +43,10 @@ public class Domain implements Serializable {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
