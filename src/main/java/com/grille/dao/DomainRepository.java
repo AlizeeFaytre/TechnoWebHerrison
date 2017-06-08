511e7b4cd0b073f9a5cd73b23aeef189633a0527
ThomasRichardISEP <tho-richard@sfr.fr>
2017-06-08 15:20:47 +0200

    modification page et controller agenda responsable module, test fonction Alizee

--- src/main/java/com/grille/dao/DomainRepository.java
+++ src/main/java/com/grille/dao/DomainRepository.java
@@ -12,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DomainRepository extends JpaRepository<Domain, Integer> {
 
     public Domain findOneByName(String name);
+
+    public Domain findById(int id);
 }
