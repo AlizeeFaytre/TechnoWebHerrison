d9a6ad0c952c983792b8404357e8ff03300390de
AlizeeFaytre <afaytre@juniorisep.com>
2017-06-11 12:51:02 +0200

    le tableau de recap des grilles dans gestions grille s'affiche correctement

--- src/main/java/com/grille/controller/responsableModule/GestionDomainController.java
+++ src/main/java/com/grille/controller/responsableModule/GestionDomainController.java
@@ -1,7 +1,9 @@
 package com.grille.controller.responsableModule;
 
 import com.grille.dao.DomainRepository;
+import com.grille.dao.GridRepository;
 import com.grille.entities.Domain;
+import com.grille.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Thomas on 09/05/2017.
@@ -21,10 +23,61 @@ public class GestionDomainController {
     @Autowired
     private DomainRepository domainrepo;
 
+    @Autowired
+    private GridRepository gridRepository;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/gestion_grille")
     public String index (Model model){
         List<Domain> listDomain = domainrepo.findAll();
         model.addAttribute("listDomain", listDomain);
+        Map<String, Set<Domain>> domainByPromo = new HashMap<>();
+
+        String currentSemester = userService.getCurrentSemester();
+        String[] semester = currentSemester.split(" - ");
+
+        if (semester[1] == "S1"){
+            Integer year = Integer.parseInt(semester[0]);
+            domainByPromo.put("I1", gridRepository.findByPromo(Integer.toString(year + 5)).getListDomain());
+            domainByPromo.put("I2", gridRepository.findByPromo(Integer.toString(year + 4)).getListDomain());
+            domainByPromo.put("A1", gridRepository.findByPromo(Integer.toString(year + 3)).getListDomain());
+            domainByPromo.put("A2", gridRepository.findByPromo(Integer.toString(year + 2)).getListDomain());
+            domainByPromo.put("A3", gridRepository.findByPromo(Integer.toString(year + 1)).getListDomain());
+        }
+        else {
+            Integer year = Integer.parseInt(semester[0]);
+            for (int i = 4; i > 0; i--){
+                if (!(gridRepository.findByPromo(Integer.toString(year + i)) == null)){
+                    switch (i){
+                        case 4:
+                            domainByPromo.put("I1", gridRepository.findByPromo(Integer.toString(year + 4)).getListDomain());
+                            break;
+
+                        case 3:
+                            domainByPromo.put("I2", gridRepository.findByPromo(Integer.toString(year + 3)).getListDomain());
+                            break;
+
+                        case 2:
+                            domainByPromo.put("A1", gridRepository.findByPromo(Integer.toString(year + 2)).getListDomain());
+                            break;
+
+                        case 1:
+                            domainByPromo.put("A2", gridRepository.findByPromo(Integer.toString(year + 1)).getListDomain());
+                            break;
+
+                        default:
+                            domainByPromo.put("A3", gridRepository.findByPromo(Integer.toString(year)).getListDomain());
+                            break;
+                    }
+                }
+            }
+
+        }
+
+        model.addAttribute("domainByPromo", domainByPromo);
+
         return "respoModule/gestion_grille";
     }
 }
